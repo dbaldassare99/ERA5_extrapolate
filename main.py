2f5efa8e8351619539938bfc8845ed440e8ebcc1
dbaldassare99 <120267926+dbaldassare99@users.noreply.github.com>
2024-02-02 17:04:49 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import xarray as xr
+import numpy as np
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as st
+import statsmodels.api as sm
+import seaborn as sns
+
